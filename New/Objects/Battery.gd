e4ca5a7d292af1a7ded0d70cce6e2448798ceab2
Pinggou-C <milomatiscornelissen@gmail.com>
2021-08-30 09:17:53 +0200

    Pinggou

---
+++ New/Objects/Battery.gd
@@ -0,0 +1,23 @@
+extends StaticBody
+
+var posconnect = false
+var negconnect = false
+func _ready():
+	pass 
+
+
+
+func posdiscon(body):
+	pass # Replace with function body.
+
+
+func negcon(body):
+	pass # Replace with function body.
+
+
+func negdiscon(body):
+	pass # Replace with function body.
+
+
+func poscon(body):
+	pass # Replace with function body.
