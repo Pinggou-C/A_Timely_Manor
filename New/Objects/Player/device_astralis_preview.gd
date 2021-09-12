0916a141ff1ca21606c6c4bcaff9fd1425e867e6
Pinggou-C <milomatiscornelissen@gmail.com>
2021-09-12 22:27:39 +0200

    pinggou

--- New/Objects/Player/device_astralis_preview.gd
+++ New/Objects/Player/device_astralis_preview.gd
@@ -9,5 +9,5 @@ extends Sprite
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(OS.get_window_size()/2)
-	position = Vector2(2560, 1600)/2
+	position = OS.get_window_size()/2
 	pass
