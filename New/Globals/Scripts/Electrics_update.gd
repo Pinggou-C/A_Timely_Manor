77da068773b02b5e903c8576a2e59acb77370bab
Pinggou-C <64211194+Pinggou-C@users.noreply.github.com>
2021-10-11 12:13:12 +0200

    pinggou

--- New/Globals/Scripts/Electrics_update.gd
+++ New/Globals/Scripts/Electrics_update.gd
@@ -16,6 +16,7 @@ var updated_components = 0
 
 var first_node
 var errors = []
+var errorsid = []
 var starting_point 
 
 var connecteds = []
@@ -43,13 +44,56 @@ func update_all_electrical_components():
 	# Update all components, waiting for a frame every time the cap is reached
 	var scene_tree = get_tree()
 	var components_updated_this_frame = 0
-	for i in scene_tree.get_nodes_in_group("electrical_components"):
+	#checks batteries, appliances and one way things
+	for i in scene_tree.get_nodes_in_group("electrical_components_special"):
 		var p = i.conecteds()
+		var err = false
 		if p[2] != "error":
-			connecteds.append([p[0], p[1], p[2], p[3]])
+			var nodes = p[1]
+			if !errorid.has(p[0]):
+				for node in nodes:
+					if node.is_in_group("battery") || node.is_in_group("appliance"):
+						var side
+						if p[0] == node.front:
+							side = "front"
+						elif p[0] == node.rear:
+							side = "rear"
+						if p[1][0] == node && side == "front":
+							errors.append([i, node, "facing towards"])
+							errorsid.append(node)
+							err = true
+						elif p[1][1] == node && side == "rear":
+							errors.append([i, node, "facing towards"])
+							errorsid.append(node)
+							err = true
+			if err = "false":
+				connecteds.append([p[0], p[1], p[2], p[3]])
+				nodes.append(p[0])
+				if p[0].is_in_group("batteries"):
+				conned_batteries.append([p[0]])
+			components_updated_this_frame += 1
+			if components_updated_this_frame == MAX_COMPONENT_UPDATES_PER_FRAME:
+				# Yield until the next physics frame, allowing other parts of your code to execute.	
+				yield(scene_tree, "physics_frame") 
+				# Or "idle_frame", depending on which suits your game better
+				components_updated_this_frame = 0
+	for i in scene_tree.get_nodes_in_group("electrical_components_normal"):
+		var p = i.conecteds()
+		var err = false
+		var points = []
+		if p[2] != "error":
+			var nodes = p[1]
+			for node in nodes:
+				ver err = false
+				if node.is_in_group("battery") || node.is_in_group("appliance"):
+					if errorid.has(node) || errorid.has(node.front) || errorid.has(node.rear):
+						err = true
+				if err == false:
+					points.append(node)
+			connecteds.append([p[0], points, p[2], p[3]])
 			nodes.append(p[0])
 			if p[0].is_in_group("batteries"):
-				conned_batteries.append([p[0]])
+			conned_batteries.append([p[0]])
 			components_updated_this_frame += 1
 			if components_updated_this_frame == MAX_COMPONENT_UPDATES_PER_FRAME:
 				# Yield until the next physics frame, allowing other parts of your code to execute.	
